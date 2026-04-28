16ff63f9676d7f42c67debc3da4e956ebad08118
darwinjoelap <darwinjoelap@gmail.com>
2026-04-28 15:23:18 -0400

    Crear superusuario automaticamente al iniciar

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,7 +1,28 @@
-from django.apps import AppConfig
+﻿from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     verbose_name = 'Cuentas de usuario'
+
+    def ready(self):
+        import os
+        if os.environ.get('DJANGO_SUPERUSER_USERNAME'):
+            try:
+                from django.contrib.auth import get_user_model
+                User = get_user_model()
+                username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+                password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+                email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(
+                        username=username,
+                        password=password,
+                        email=email,
+                        rol='doctora',
+                        first_name='Vanessa',
+                        last_name='Pereira',
+                    )
+            except Exception:
+                pass
