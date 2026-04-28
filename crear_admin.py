4afa0b59aa0be837978435fc94b45a84812e3587
darwinjoelap <darwinjoelap@gmail.com>
2026-04-28 10:21:16 -0400

    Agregar superusuario y railway.toml

---
+++ crear_admin.py
@@ -0,0 +1,16 @@
+﻿import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consultorio.settings')
+import django
+django.setup()
+from accounts.models import Usuario
+if not Usuario.objects.filter(username='admin').exists():
+    Usuario.objects.create_superuser(
+        username='admin',
+        password='CambiarEsto2024!',
+        rol='doctora',
+        first_name='Vanessa',
+        last_name='Pereira',
+    )
+    print('Superusuario creado')
+else:
+    print('Ya existe')
